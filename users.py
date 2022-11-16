5bd58b00192d96a6a8f647b24b371b9fd0338a3e
zalan0815 <tofalvi.zalan@students.jedlik.eu>
2022-11-16 10:09:22 +0100

    #8 fixek és új cuccok

--- users.py
+++ users.py
@@ -1,4 +1,17 @@
 import os 
+import time
+from fishes_functions import check
+
+felirat = """
+   _____ ______ __                  _     __     _   __  _   _          
+  / ____|___  //_/                 | |   /_/    (_) /_/ | | | |         
+ | (___    / / __ _ _ __ ___   ___ | |_   _ _ __ _  __ _| | | |__   ___ 
+  \___ \  / / / _` | '_ ` _ \ / _ \| __| | | '__| |/ _` | | | '_ \ / _ \
+  ____) |/ /_| (_| | | | | | | (_) | |_  | | |  | | (_| | | | |_) |  __/
+ |_____//_____\__,_|_| |_| |_|\___/ \__| |_|_|  | |\__,_|_| |_.__/ \___|
+                                               _/ |                     
+                                              |__/                      
+"""
 
 files = os.listdir()
 files.pop(0)
@@ -12,22 +25,41 @@ def list_users():
             users.append(type[0])
 
     count = 1
+    os.system("cls")
+    print("--------------------------------------------------------------------------------")
+    print("Profilok:")
     for u in users:
         print(f'{count} - {u}')
         count += 1
     print(f'\n{count} - Új felhasználó regisztrálása')
     print(f'\n0 - Kilépés')
+    print("--------------------------------------------------------------------------------")
 
 def new_user():
+    os.system("cls")
+    print("--------------------------------------------------------------------------------")
+    print("Új felhasználó létrehozása")
+    print('0 - Mégse')
     user_name = input('Felhasználó név: ')
+    if user_name == '0':
+        user()
+    print("Sikeresen létrehoztuk az új profilt!")
+    print("--------------------------------------------------------------------------------")
     f = open(f'{user_name}.csv', 'w', encoding='UTF-8')
     f.write('Name;Mass;Length\n')
     f.close()
 
 def user():
+    os.system("cls")
     list_users()
-    choice = int(input('Választás: '))
-    if choice > 0 and choice <= len(users):
-        return f'{users[choice - 1]}.csv'
-    elif choice > len(users):
+    choice = input('Választás: ')
+    while check(choice) == False:
+        os.system("cls")
+        print(f"\033[91m{felirat}\033[0m")
+        time.sleep(10)
+        user()
+    if check(choice) > 0 and check(choice) <= len(users):
+        return f'{users[int(choice) - 1]}.csv'
+    elif check(choice) > len(users):
         new_user()
+user()
