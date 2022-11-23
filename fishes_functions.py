ea0367fffbbe3c613b3096ca77c9c3e9bc14bd77
p4likas51 <schmitzhofer.pal@students.jedlik.eu>
2022-11-23 12:10:12 +0100

    ujra... #7

--- fishes_functions.py
+++ fishes_functions.py
@@ -122,7 +122,3 @@ def list_fishes():
         elif d.endangered == 'Y':
             endangered = 'védett'
         print(f'{d.name} ({d.min_mass}kg - {d.max_mass}kg, {d.min_length}m - {d.max_length}m, {endangered})')
-
-
-
-modify_fish()
