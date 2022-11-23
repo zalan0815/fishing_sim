e9c5c66d839d93a2be41b9fb85f65f08a358d6f8
p4likas51 <schmitzhofer.pal@students.jedlik.eu>
2022-11-23 12:09:45 +0100

    #7

--- fishes_functions.py
+++ fishes_functions.py
@@ -77,15 +77,23 @@ def modify_fish():
         if d.name.lower() == name.lower():
             d.min_mass = input('Minimum súly: ')
             while check(d.min_mass) == False:
+                print(felirat)
+                time.sleep(2)
                 d.min_mass = input('Minimum súly: ')
             d.max_mass = input('Maximum súly: ')
             while check(d.max_mass) == False:
+                print(felirat)
+                time.sleep(2)
                 d.max_mass = input('Maximum súly: ')
             d.min_length = input('Minimum hossz: ')
             while check(d.min_length) == False:
+                print(felirat)
+                time.sleep(2)
                 d.min_length = input('Minimum hossz: ')
             d.max_length = input('Maximum hossz: ')
             while check(d.max_length) == False:
+                print(felirat)
+                time.sleep(2)
                 d.max_length = input('Maximum hossz: ')
             d.endangered = input('Védettségi státusz: ').upper()
             write_file()
@@ -117,3 +125,4 @@ def list_fishes():
 
 
 
+modify_fish()
