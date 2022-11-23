3e8ba023957914def5cdc53cdb48d7e9caab04ca
p4likas51 <schmitzhofer.pal@students.jedlik.eu>
2022-11-23 10:26:30 +0100

    felirat

--- fishes_functions.py
+++ fishes_functions.py
@@ -46,13 +46,13 @@ def new_fish():
     while check(min_length) == False:
         print(felirat)
         time.sleep(2)
-        max_mass = input('Hal minimum hossza: ')
+        min_length = input('Hal minimum hossza: ')
     max_length = input('Hal maximum hossza: ')
     while check(max_length) == False:
         print(felirat)
         time.sleep(2)
-        max_mass = input('Hal maximum hossza: ')
-    endangered = input('Hal védettsége: ')
+        max_length = input('Hal maximum hossza: ')
+    endangered = input('Hal védettsége (Y/N): ').upper()
     
     
     
@@ -82,6 +82,3 @@ def list_fishes():
         elif d.endangered == 'Y':
             endangered = 'védett'
         print(f'{d.name} ({d.min_mass}kg - {d.max_mass}kg, {d.min_length}m - {d.max_length}m, {endangered})')
-
-
-new_fish()
