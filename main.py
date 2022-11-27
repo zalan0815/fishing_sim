1620f59ec530469af7c4a27b7ec19d81cf2c93cc
“Zalán <tofalvi.zalan@students.jedlik.eu>
2022-11-27 11:42:40 +0100

    teszt

--- main.py
+++ main.py
@@ -84,3 +84,9 @@ menu()
     
 
  
+#Üdvözlő a bejelntkezés előtt
+#A horgászás kicsit lassú
+#Egy cucc megcsinálása után visszadob a legelső menübe nem pedig abba a menübe
+#Nincs kilépés a játékból 
+#A halállomány szerkesztésnél nem listázza ki mielőtt be kell írni a nevet
+#A halaim keresésénél 2 enter kell
